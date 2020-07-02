338a17fa9485bb876cff5833598709b90fbc847e
Szpoti <gtagigafan@gmail.com>
2020-07-02 15:51:38 +0200

    Symbol display and api added

---
+++ MagicApi/Services/Models/SymbolModel.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System;
+namespace Services.Models
+{
+    public class SymbolModel
+    {
+        public string symbol { get; set; }
+        public string svg_uri { get; set; }
+        public string loose_variant { get; set; }
+        public string english { get; set; }
+        public bool transposable { get; set; }
+        public bool represents_mana { get; set; }
+        public bool appears_in_mana_costs { get; set; }
+        public string cmc { get; set; }
+        public bool funny { get; set; }
+        public List<object> colors { get; set; }
+        public object gatherer_alternates { get; set; }
+
+    }
+
+    public class Root
+    {
+        public bool has_more { get; set; }
+        public List<SymbolModel> data { get; set; }
+
+    }
+}
