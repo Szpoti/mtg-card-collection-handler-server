36b1ca7ce2b93eaee01ad24d3b71128718053e17
Szpoti <gtagigafan@gmail.com>
2021-02-09 09:22:25 +0100

    Basic DeckController created

---
+++ MagicApi/Controllers/DeckController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Microsoft.Extensions.Options;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using MagicApi.Models;
+using MagicApi.Services;
+using MagicApi.Data;
+using BCrypt.Net;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace MagicApi.Controllers
+{
+    [Route("api/deck")]
+    [ApiController]
+    public class DeckController : ControllerBase
+    {
+        private readonly MTGContext _context;
+
+        private readonly IConfiguration _configuration;
+
+
+        public DeckController(MTGContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+    }
+}
